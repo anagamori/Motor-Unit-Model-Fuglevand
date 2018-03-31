0133792f396db0520a671e7b83857a682a782e13
Akira <nagamori@usc.edu>
2018-03-30 20:59:39 -0700

    Update on model

--- AnalysisCode_MU.m
+++ AnalysisCode_MU.m
@@ -2,44 +2,66 @@ close all
 clear all
 clc
 
-dataFolder = '/Volumes/DATA2/TwoMuscleSystemData/MotorUnit/ActivationLevel';
-codeFolder = '/Users/akira/Documents/Github/Afferented-Muscle/Two-muscle System/Combined Model';
+data_directory = '/Volumes/DATA2/Motor Unit Model Data/Fuglevand/';
+code_directory = '/Users/akira/Documents/Github/Motor-Unit-Model-Fuglevand/';
 %load ('Input')
 Fs = 1000;
-t = 0:1/Fs:15;
-amp_temp = 0.05:0.05:1;
+t = 0:1/Fs:10;
+amp_temp = 0.1:0.1:1;
 CoVAll = zeros(10,length(amp_temp));
+meanForceAll = zeros(10,length(amp_temp));
+stdAll = zeros(10,length(amp_temp));
 pxxAll = zeros(length(amp_temp),301);
 
 for k = 1:length(amp_temp)
-    trialN = k+20; 
+    trialN = k+30; 
     % predefine model parameters
     
     CoV = zeros(1,10);
+    std_Force = zeros(1,10);
+    mean_Force = zeros(1,10);
     pxx = zeros(10,301);
-    cd (dataFolder)
+    cd (data_directory)
     load(['Trial_' num2str(trialN)])
-    cd (codeFolder)
+    cd (code_directory)
     for i = 1:10
         output = Data{i};
         Force = output.TotalForce(end-5*Fs+1:end);
+        mean_Force(i) = mean(Force);
+        std_Force(i) = std(Force);
         CoV(i) = std(Force)/mean(Force);
         [pxx(i,:),f] = pwelch(Force-mean(Force),gausswin(5*Fs),2.5*Fs,0:0.1:30,Fs,'power');
         pxx(i,:) = pxx(i,:)./sum(pxx(i,:));
         pxx(i,:) = smooth(pxx(i,:),10);
     end
+    
+    meanForceAll(:,k) = mean_Force;
+    stdAll(:,k) =  std_Force;
     CoVAll(:,k) =  CoV;
     pxxAll(k,:) = mean(pxx);
     
 end
 
+meanForce_plot = mean(meanForceAll)./mean(meanForceAll(:,end));
 figure(1)
-plot(amp_temp,mean(CoVAll*100))
+plot([0 amp_temp*100],[0 meanForce_plot*100])
+xlabel('Excitatory Drive (%)')
+ylabel('Force (%)')
 
 figure(2)
+plot(meanForce_plot*100,mean(CoVAll*100))
+xlabel('Excitatory Drive (%)')
+ylabel('CoV (%)')
+
+figure(3)
+plot(meanForce_plot*100,mean(stdAll))
+xlabel('Excitatory Drive (%)')
+ylabel('SD (AU)')
+
+figure(4)
 plot(f,pxxAll)
 
-cd (dataFolder)
-save('CoV_300','CoVAll')
-save('pxx_300','pxxAll')
-cd (codeFolder)
+% cd (dataFolder)
+% save('CoV_300','CoVAll')
+% save('pxx_300','pxxAll')
+% cd (codeFolder)
--- runMotorUnitModel.m
+++ runMotorUnitModel.m
@@ -3,19 +3,22 @@ clear all
 clc
 
 Fs = 1000;
-t = 0:1/Fs:5;
+t = 0:1/Fs:10;
 N_temp = 50:50:1000;
-amp_temp = 0.05:0.05:1;
+amp_temp = 0.1:0.1:1;
 RP_temp = 10:10:150;
 
-for k = 1
-    trialN = k; 
+data_directory = '/Volumes/DATA2/Motor Unit Model Data/Fuglevand/';
+code_directory = '/Users/akira/Documents/Github/Motor-Unit-Model-Fuglevand/';
+
+for k = 1:length(amp_temp)
+    trialN = k+30; 
     % predefine model parameters
-    amp = 0.2;
+    amp = amp_temp(k);
     modelParameter.amp = amp;
     U = [zeros(1,1*Fs) (amp/2)*(0:1/Fs:2) amp*ones(1,length(t)-3*Fs-1)];
     
-    modelParameter.N = 120;    
+    modelParameter.N = 300;    
     modelParameter.RR = 30;    
     modelParameter.MFR = 8;   
     modelParameter.g_e = 1;    
@@ -25,31 +28,32 @@ for k = 1
     modelParameter.RP = 100;    
     modelParameter.T_L = 90;    
     modelParameter.RT = 3;   
-    modelParameter.P_amp = 0.03;
+    
     
     Data = cell(1,10);
-    for i = 1
-        % Run motor unit model
+    tic
+    parfor i = 1:10
+        % Run motor unit model        
         output = MotorUnitModel(t,U,modelParameter,Fs);       
         Data{i} = output;
     end
+    toc
+    
+    cd (data_directory)
+    save(['Trial_' num2str(trialN)],'Data','-v7.3')
+    cd (code_directory)
     
     output_temp = Data{1};
+    
+    Force = output_temp.TotalForce(4*Fs+1:end);
+    meanForce = mean(Force)
+    SD = std(Force)
+    CoV = std(Force)/mean(Force)
+
     figure(1)
     plot(t,output_temp.TotalForce)
+    xlabel('Time (s)','FontSize',14)
+    ylabel('Force (AU)','FontSize',14)
     hold on
     k
 end
-
-CS = sum(output.SpikeTrain);
-Force = output_temp.TotalForce(4*Fs+1:end);
-CoV = std(Force)/mean(Force)
-[pxx,f] = pwelch(Force-mean(Force),[],[],0:0.1:30,Fs,'power');
-[pxx2,f] = pwelch(CS-mean(CS),[],[],0:0.1:30,Fs,'power');
-figure(2)
-plot(f,pxx)
-
-figure(3)
-plot(f,pxx2)
-
-
